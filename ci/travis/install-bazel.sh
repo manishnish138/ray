167fafc93a345a64c1269b23e2620bcb4e428ed4
manishnish138 <82148691+manishnish138@users.noreply.github.com>
2021-05-27 19:06:10 +0530

    Update install-bazel.sh

--- ci/travis/install-bazel.sh
+++ ci/travis/install-bazel.sh
@@ -55,6 +55,7 @@ elif [ "$(uname -m)" = "aarch64" ]; then
   wget -O "bazel-linux-arm64" "https://github.com/bazelbuild/bazel/releases/download/${version}/bazel-${version}-${platform}-arm64"
   chmod +u bazel-linux-arm64
   sudo mv bazel-linux-arm64 /usr/bin/bazel
+  whereis bazel
 else
   target="./install.sh"
   curl -f -s -L -R -o "${target}" "https://github.com/bazelbuild/bazel/releases/download/${version}/bazel-${version}-installer-${platform}-${achitecture}.sh"
